95f5f00cca21ae393aea9b4e30bd44c246efd9db
bha <accountfor169@yahoo.com>
2014-11-08 09:17:30 +0000

    idk

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+    var text = fs.readFileSync('index.html','utf8')
+  response.send(text.toString())
 })
 
 app.listen(app.get('port'), function() {
